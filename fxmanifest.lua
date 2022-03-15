404fedb706bb34fb17d93c5abf8129d19623ee6e
BlackzoneV <sergen-1250@hotmail.com>
2022-03-15 18:21:22 +0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'adamant'
+game 'gta5'
+
+Author 'Blackzone#1020'
+description 'Anti Fire Rate'
+
+files {
+  'common/data/ai/*.meta',
+}
+
+
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weapons.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weaponheavypistol.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weaponmachinepistol.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weaponminismg.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weapons_doubleaction.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weapons_pistol_mk2.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weapons_revolver_mk2.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weapons_snspistol_mk2.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weaponsnspistol.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weaponvintagepistol.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weapons_assaultrifle_mk2.meta'
+data_file 'WEAPON_METADATA_FILE' 'common/data/ai/weaponcompactrifle.meta'
+data_file 'WEAPON_ANIMATIONS_FILE' 'common/data/ai/weaponanimations.meta'
+data_file 'WEAPONCOMPONENTSINFO_FILE' 'common/data/ai/weaponcomponents.meta'
+data_file 'VEHICLE_METADATA_FILE' 'common/data/ai/vehicleaihandlinginfo.meta'
